57f247ea3cf5a51959a37446302cb4932fff04e0
Lucas Rezende <macedolucas@hotmail.com>
2016-05-07 18:22:46 -0300

    finalizacao do xml base de uma atracao

--- app/src/main/java/com/unb/struct_trainees/plataformaeventosapp/AtracaoActivity.java
+++ app/src/main/java/com/unb/struct_trainees/plataformaeventosapp/AtracaoActivity.java
@@ -1,13 +1,20 @@
 package com.unb.struct_trainees.plataformaeventosapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class AtracaoActivity extends AppCompatActivity {
+    String titulo = "Sobre";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_atracao);
+
+        TextView tv = (TextView) findViewById(R.id.atracao_text_titulo);
+        tv.setText(titulo);
     }
 }
