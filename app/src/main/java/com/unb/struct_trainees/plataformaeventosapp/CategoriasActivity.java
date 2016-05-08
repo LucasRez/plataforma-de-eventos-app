3a674729e1d429fb8ac3701864e316e148842430
ChrisCWerner <ccwerner96@gmail.com>
2016-05-08 15:35:23 -0300

    Adicionado o layout de categorias.

---
+++ app/src/main/java/com/unb/struct_trainees/plataformaeventosapp/CategoriasActivity.java
@@ -0,0 +1,17 @@
+package com.unb.struct_trainees.plataformaeventosapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ *
+ * Created by Chris on 08/05/2016.
+ */
+public class CategoriasActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_categorias);
+    }
+}
