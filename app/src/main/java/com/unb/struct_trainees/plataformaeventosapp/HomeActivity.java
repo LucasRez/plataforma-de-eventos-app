502da9acc72208d9a64dc3fba7662ed83a49fe75
ChrisCWerner <ccwerner96@gmail.com>
2016-06-06 19:18:09 -0300

    Adicionado a toolbar.

---
+++ app/src/main/java/com/unb/struct_trainees/plataformaeventosapp/HomeActivity.java
@@ -0,0 +1,17 @@
+package com.unb.struct_trainees.plataformaeventosapp;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Chris on 06/06/2016.
+ */
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_home);
+    }
+}
